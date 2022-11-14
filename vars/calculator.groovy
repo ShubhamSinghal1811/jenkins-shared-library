be926630aaa6402aa7253c42f87b14fac0124d66
ShubhamSinghal1811 <shubham.singhal1811@gmail.com>
2022-11-14 16:37:45 +0530

    f

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,6 +1,7 @@
 def call(var1,var2,ch){
     if (${ch} == "+"){
        sh "res=`echo ${var1} + ${var2} | bc`"
-       sh "echo $res"
+       sh "echo ${res}"
     }
+    // sh "res=`echo ${var1} ${ch} ${var2} | bc`"
 }
