e7138bbc016f99c9fc2336bf4dbe151f790050a3
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 11:34:39 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,1 +1,39 @@
-ecr.groovy
+def call(){
+environment {
+AWS_ACCOUNT_ID="982923341761"
+AWS_DEFAULT_REGION="ap-south-1"
+IMAGE_REPO_NAME="demoappf311c80d/helloworldfunction19d43fc4repo"
+IMAGE_TAG="latest"
+REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}"
+}
+  stages {
+        
+        stage('Logging into AWS ECR') {
+steps {
+sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+}
+}
+
+stage('Building image') {
+steps{
+script {
+sh 'docker build -t "${IMAGE_REPO_NAME}:${IMAGE_TAG}" ./hello-world'
+}
+}
+}
+
+stage('Pushing to ECR') {
+steps{
+script {
+sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
+sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+}
+}
+}
+
+
+
+ 
+    
+    }
+}
