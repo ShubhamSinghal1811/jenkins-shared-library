57edc29dd2306b709a2dd1dd7fa905f718a0ddda
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 12:13:22 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,13 +1,8 @@
 def call(){
-sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-
-
-sh 'docker build -t "${IMAGE_REPO_NAME}:${IMAGE_TAG}" ./hello-world'
-
-
-
-sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
-sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+sh "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 982923341761.dkr.ecr.ap-south-1.amazonaws.com"
+sh 'docker build -t "demoappf311c80d/helloworldfunction19d43fc4repo:latest1" ./hello-world'
+sh "docker tag demoappf311c80d/helloworldfunction19d43fc4repo:latest1 982923341761.dkr.ecr.ap-south-1.amazonaws.com/demoappf311c80d/helloworldfunction19d43fc4repo:latest1"
+sh "docker push 982923341761.dkr.ecr.ap-south-1.amazonaws.com/demoappf311c80d/helloworldfunction19d43fc4repo:latest1"
 }
 
 
