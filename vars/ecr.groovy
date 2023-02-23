d5c2ec52ad54197d59fde58c85b0517438f739c0
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 13:54:43 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 982923341761.dkr.ecr.ap-south-1.amazonaws.com"
+sh 'echo "hello"'
 
 
    
