ca0c7dd00221e36afe307a8803fce909b280f1d1
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 11:56:16 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,33 +1,16 @@
 def call(){
-
-  stages {
-        
-        stage('Logging into AWS ECR') {
-steps {
 sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-}
-}
 
-stage('Building image') {
-steps{
-script {
+
 sh 'docker build -t "${IMAGE_REPO_NAME}:${IMAGE_TAG}" ./hello-world'
-}
-}
-}
 
-stage('Pushing to ECR') {
-steps{
-script {
+
+
 sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
 sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
-}
-}
 }
 
 
 
  
     
-    }
-}
