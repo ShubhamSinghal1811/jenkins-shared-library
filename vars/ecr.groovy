91e8dc343684506bb287b2cfb69915e775fb9c14
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 12:16:27 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,8 +1,5 @@
 def call(){
 sh "aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 982923341761.dkr.ecr.ap-south-1.amazonaws.com"
-sh 'docker build -t "demoappf311c80d/helloworldfunction19d43fc4repo:latest1" ./hello-world'
-sh "docker tag demoappf311c80d/helloworldfunction19d43fc4repo:latest1 982923341761.dkr.ecr.ap-south-1.amazonaws.com/demoappf311c80d/helloworldfunction19d43fc4repo:latest1"
-sh "docker push 982923341761.dkr.ecr.ap-south-1.amazonaws.com/demoappf311c80d/helloworldfunction19d43fc4repo:latest1"
 }
 
 
