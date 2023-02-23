cd1df426421096ce10e2095d2c1c0177c8ea7105
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2023-02-23 17:58:54 +0530

    Update ecr.groovy

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -18,7 +18,7 @@ REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/
         
         stage('Logging into AWS ECR') {
 steps {
-sh "sudo aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+sh "sudo aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | sudo docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 }
 }
 
